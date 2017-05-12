aec6a33c1945c33adf8e1d3dba8060928168b654
chanf2824 <chanflorence27@gmail.com>
2017-05-12 10:38:25 -0700

    Created database, Added Name field, Added button to add record to database

--- MyContactApp/app/src/main/java/com/example/chanf/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/chanf/mycontactapp/DatabaseHelper.java
@@ -15,17 +15,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, null, version);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID_INTEGER_PRIMARY_KEY_AUTOINCREMENT, NAME TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }

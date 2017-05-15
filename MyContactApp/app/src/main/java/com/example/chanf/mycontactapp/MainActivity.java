ae794806f435aaf42674ce404f95a9c00006ec95
chanf2824 <chanflorence27@gmail.com>
2017-05-15 11:12:46 -0700

    Added new EditText variables and added constraints; added new text fields on layout

--- MyContactApp/app/src/main/java/com/example/chanf/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/chanf/mycontactapp/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.chanf.mycontactapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editNumber;
+    EditText editAddress;
     Button btnAddData;
 
 
@@ -23,19 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
         //Add the layout vars
         editName = (EditText)findViewById(R.id.editText_name);
+        editNumber = (EditText)findViewById(R.id.editText_number);
+        editAddress = (EditText)findViewById(R.id.editText_address);
 
     }
 
     public void addData(View v){
         boolean isInserted = myDb.insertData(editName.getText().toString());
+        Context context = getApplicationContext();
+        CharSequence text;
+
 
         if(isInserted == true){
             Log.d("MyContact", "Success inserting data");
             //Insert Toast message here.....
+            text = "Data inserted!";
+            Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            toast.show();
         }
         else {
             Log.d("MyContact", "Failure inserting data");
+            text = "Data couldn't be inserted";
             //Insert Toast message here.....
+            Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 }
